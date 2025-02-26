bc7f0120838b892f59c2fc99154fcbb480e7db32
IvanSobolev <i@bezlikiy.ru>
2025-02-26 13:06:01 +0300

    feat: interfaces for repository

---
+++ KODPersonalAccount/Models/OperationResult.cs
@@ -0,0 +1,7 @@
+﻿namespace KODPersonalAccount.Models;
+
+public class OperationResult (bool isSuccess, string? error = default)
+{
+    public bool IsSuccess { get; set; } = isSuccess;
+    public string? ErrorMessage { get; set; } = error;
+}
