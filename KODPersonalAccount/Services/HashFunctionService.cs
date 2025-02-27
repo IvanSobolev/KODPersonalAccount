87a56574983e09ba9c299f14467cb8ebf4e18d70
IvanSobolev <i@bezlikiy.ru>
2025-02-27 21:07:22 +0300

    feat: hash function service

---
+++ KODPersonalAccount/Services/HashFunctionService.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace KODPersonalAccount.Services;
+
+public static class HashFunctionService
+{
+    public static string ComputeHmacSha256(string key, string data)
+    {
+        using var hmac = new HMACSHA256(Encoding.UTF8.GetBytes(key));
+        var hashBytes = hmac.ComputeHash(Encoding.UTF8.GetBytes(data));
+        return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+    }
+}
