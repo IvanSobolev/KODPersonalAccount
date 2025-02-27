7bbcdf7d74cfc2cc9a0874882948101516402f9e
IvanSobolev <i@bezlikiy.ru>
2025-02-27 21:07:39 +0300

    feat: interface for telegram auth service

---
+++ KODPersonalAccount/Interfaces/Services/ITelegramAuthService.cs
@@ -0,0 +1,10 @@
+﻿using KODPersonalAccount.Models.DTO;
+
+namespace KODPersonalAccount.Interfaces.Services;
+
+public interface ITelegramAuthService
+{
+    Task<TelegramUser> GetUserFromInitData(string initData);
+    Task<bool> ValidateInitData(string initData, string botToken);
+    Task<TelegramUser> ExtractUserFromInitData(string initData);
+}
