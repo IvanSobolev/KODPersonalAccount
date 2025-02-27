84da7ee47530260b1bc31504d82f4b22b99813ef
IvanSobolev <i@bezlikiy.ru>
2025-02-27 21:36:46 +0300

    feat: interface for token service and token output DTO

---
+++ KODPersonalAccount/Interfaces/Services/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using KODPersonalAccount.Models.DTO;
+
+namespace KODPersonalAccount.Interfaces.Services;
+
+public interface IAuthService
+{
+    Task<TokenOutputDto?> Auth(string initData);
+    Task<TokenOutputDto?> RefreshToken(string refreshToken);
+}
