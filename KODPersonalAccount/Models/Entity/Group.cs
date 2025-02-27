8d0c8291ec2dbdd69f6a6fff76af05f8bfe299c7
Nikita Matigorov <r.matigorov@yandex.ru>
2025-02-27 23:49:20 +0300

    Изменен Group.cs

--- KODPersonalAccount/Models/Entity/Group.cs
+++ KODPersonalAccount/Models/Entity/Group.cs
@@ -1,17 +1,106 @@
 ﻿namespace KODPersonalAccount.Models.Entity;
 
+/// <summary>
+/// Группа.
+/// </summary>
 public class Group
 {
-    public long Id { get; set; }
-    public bool IsPrivate { get; set; }
-    public string Color { get; set; }
+    /// <summary>
+    /// Идентификатор.
+    /// </summary>
+    public Guid Id { get; init; }
+    
+    /// <summary>
+    /// Год обучения.
+    /// </summary>
     public int StudyYears { get; set; }
 
-    public long DirectionId { get; set; }
-    public long TeacherId { get; set; }
+    /// <summary>
+    /// Расписание занятий.
+    /// </summary>
+    public string? Schedule { get; set; }   
     
-    public Direction Direction { get; set; }
-    public User Teacher { get; set; }
-    public ICollection<UserToGroup> UserToGroups { get; set; }
-    public ICollection<Lesson> Lessons { get; set; }
+    /// <summary>
+    /// Идентификатор направления.
+    /// </summary>
+    public long DirectionId { get; set; }
+    
+    /// <summary>
+    /// Идентификатор педагога.
+    /// </summary>
+    public long? TeacherId { get; set; }
+    
+    /// <summary>
+    /// Идентификатор студентов.
+    /// </summary>
+    public List<long>? StudentIds { get; set; }
+
+    /// <summary>
+    /// Создание на основе первичных данных.
+    /// </summary>
+    /// <param name="id">Идентификатор.</param>
+    /// <param name="schedule">Расписание занятий.</param>
+    /// <param name="directionId">Идентификатор направления.</param>
+    /// <param name="teacherId">Идентификатор педагога.</param>
+    /// <param name="studentIds">Идентификатор студентов.</param>
+    /// <param name="studyYears">Год обучения.</param>
+    public Group(
+        Guid id,
+        string? schedule,
+        long directionId,
+        long? teacherId,
+        List<long>? studentIds,
+        int studyYears = 1)
+    {
+        Id = id;
+        StudyYears = studyYears;
+        DirectionId = directionId;
+        StudentIds = new();
+
+        if (studentIds is not null)
+        {
+            SetStudentIds(
+                studentIds);
+        }
+        
+        if (teacherId is not null)
+        {
+            TeacherId = teacherId;
+        }
+
+        if (studentIds is not null)
+        {
+            SetStudentIds(
+                studentIds);
+        }
+        
+    }
+
+    /// <summary>
+    /// Установить расписание.
+    /// </summary>
+    /// <param name="schedule">Расписание.</param>
+    public void SetSchedule(
+        string schedule)
+    {
+        schedule = schedule.Trim();
+
+        if (!string.IsNullOrEmpty(schedule))
+        {
+            Schedule = schedule;
+        }
+    }
+    
+    /// <summary>
+    /// Добавить студентов.
+    /// </summary>
+    /// <param name="studentIds">Идентификаторы студентов.</param>
+    public void SetStudentIds(
+        List<long> studentIds)
+    {
+        if (studentIds.Count > 0)
+            foreach (long studentId in studentIds)
+                if (!StudentIds.Contains(studentId))
+                    StudentIds.Add(studentId);
+    }
 }
