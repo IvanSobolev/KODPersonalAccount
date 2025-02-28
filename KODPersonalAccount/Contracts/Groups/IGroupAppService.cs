3722e362b43edeea819f934df11486f219ec73be
Nikita Matigorov <r.matigorov@yandex.ru>
2025-02-28 18:37:03 +0300

    Создан и реализован IGroupAppService.cs

---
+++ KODPersonalAccount/Contracts/Groups/IGroupAppService.cs
@@ -0,0 +1,58 @@
+using KODPersonalAccount.Models.Entity;
+
+namespace KODPersonalAccount.Contracts.Groups;
+
+/// <summary>
+/// Сервис группы.
+/// </summary>
+public interface IGroupAppService
+{
+    /// <summary>
+    /// Получить группу.
+    /// </summary>
+    /// <param name="id">Идентификатор.</param>
+    /// <returns>Группа.</returns>
+    Task<Group?> GetAsync(
+        Guid id);
+    
+    /// <summary>
+    /// Получить список групп.
+    /// </summary>
+    /// <param name="studyYears">Год обучения.</param>
+    /// <param name="schedule">Расписание.</param>
+    /// <param name="directionId">Идентификатор направления.</param>
+    /// <param name="teacherId">Идентификатор педагога.</param>
+    /// <param name="studentId">Идентификатор студента.</param>
+    /// <returns>Список групп.</returns>
+    Task<List<Group>> GetListAsync(
+        int? studyYears, 
+        string? schedule, 
+        long? directionId,
+        long? teacherId,
+        long? studentId);
+    
+    /// <summary>
+    /// Создать группу.
+    /// </summary>
+    /// <param name="input">Данные для создания группы.</param>
+    /// <returns>Группа.</returns>
+    Task<Group> CreateAsync(
+        GroupCreateDto input);
+
+    /// <summary>
+    /// Обновить группу.
+    /// </summary>
+    /// <param name="id">Идентификатор.</param>
+    /// <param name="input">Данные для обновления группы.</param>
+    /// <returns>Обновленная группа.</returns>
+    Task<Group?> UpdateAsync(
+        Guid id,
+        GroupUpdateDto input);
+    
+    /// <summary>
+    /// Удалить группу.
+    /// </summary>
+    /// <param name="id">Идентификатор.</param>
+    Task DeleteAsync(
+        Guid id);
+}
